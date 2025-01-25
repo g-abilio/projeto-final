ae03f4d9371157c18177fcf156b4d54e255cace5
Gustavo Monteiro <grmonteiro@ymail.com>
2025-01-25 20:09:18 -0300

    ✨ feat: Adicionando classe abstrata de jogos de tabuleiro

---
+++ include/jogo_tabuleiro.hpp
@@ -0,0 +1,32 @@
+#ifndef JOGO_TABULEIRO_HPP
+#define JOGO_TABULEIRO_HPP
+
+#include <vector>
+#include <string>
+#include <iostream>
+
+class JogoTabuleiro {
+public:
+    // destrutor virtual para permitir destruicao correta em classes derivadas
+    virtual ~JogoTabuleiro() {}
+
+    // metodos abstratos
+    virtual void inicializar_tabuleiro() = 0;
+    virtual void imprimir_tabuleiro() const = 0;
+    virtual bool realizar_jogada(int jogador, int coluna) = 0;
+    virtual bool verificar_vitoria(int jogador) const = 0;
+
+    // metodo concreto para limpar o tabuleiro
+    void limpar_tabuleiro() {
+        for (auto& linha : tabuleiro) {
+            std::fill(linha.begin(), linha.end(), 0);
+        }
+    }
+
+protected:
+    std::vector<std::vector<int>> tabuleiro; // representacao generica do tabuleiro
+    int linhas;
+    int colunas;
+};
+
+#endif
